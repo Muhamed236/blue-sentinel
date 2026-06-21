9df4e103b792c289044616e0cecb3168100d303e
Mohamed Salah <117998830+Muhamed236@users.noreply.github.com>
2026-06-21 03:52:59 +0300

    Update sheetsWebhook URL in data.js

--- data.js
+++ data.js
@@ -58,5 +58,5 @@ window.BS_DEFAULT_DATA = {
   ],
   incidents:[],
   requests:[],
-  sheetsWebhook:''
+  sheetsWebhook:'https://script.google.com/macros/s/AKfycbyvU_Fw5myPq4TaVpHDM2DsgfuRnd6G1KzS0KdB9tex0rtThE_HOjt-SYBYsf_QM45lTQ/exec'
 };
